0efa8ae8f89eb852b3660afcb10e21878a593ea1
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-05-23 08:55:17 -0400

    Update gtag.js

--- analytics/gtag.js
+++ analytics/gtag.js
@@ -1,9 +1,6 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-51CS1LXRP9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// gtag.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-51CS1LXRP9');
-</script>
+gtag('config', 'G-51CS1LXRP9');
