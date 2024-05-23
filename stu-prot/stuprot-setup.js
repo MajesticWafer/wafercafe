e54ff0fa0a90bfbca73e47b1eaeb3f4f48a5c6fd
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-05-23 10:03:29 -0400

    Delete stu-prot directory

--- stu-prot/stuprot-setup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('save-ids').addEventListener('click', function() {
-  const input = document.getElementById('course-ids').value;
-  const courseIds = input.split(/[\s,]+/).filter(Boolean);
-  localStorage.setItem('courseIds', JSON.stringify(courseIds));
-  alert('Course IDs saved!');
-});
