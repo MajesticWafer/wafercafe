3969ec404978170365e256e538cee098d3ff05a5
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-08-21 12:20:35 -0400

    Upload N64 emulator

---
+++ webcore/n64/romlist.js
@@ -0,0 +1,6 @@
+var ROMLIST = [
+    {url:"roms/Super Mario 64.z64",title:"Super Mario 64"},
+    {url:"roms/Super Smash Bros.z64",title:"Super Smash Bros"},
+    {url:"roms/Mario Kart 64.n64",title:"Mario Kart 64"},
+    {url:"roms/Yoshi's Story.z64",title:"Yoshi's Story"},
+];
