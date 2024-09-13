b2599416c8227203342544ab4a30f04b854e9d64
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-09-13 09:11:00 -0400

    Asteroids/Flappy Bird

---
+++ flappy-bird/js/main.js
@@ -0,0 +1,34 @@
+//* ELEMENTOS PRINCIPALES DEL DOM
+
+// pantallas
+const splashScreenNode = document.querySelector("#splash-screen")
+const gameScreenNode = document.querySelector("#game-screen")
+const gameOverScreenNode = document.querySelector("#game-over-screen")
+
+// botones
+const startBtnNode = document.querySelector("#start-btn")
+
+// game box
+const gameBoxNode = document.querySelector("#game-box")
+
+
+//* VARIABLES GLOBALES DEL JUEGO
+
+
+
+
+
+
+//* FUNCIONES GLOBALES DEL JUEGO
+
+
+
+
+
+
+//* EVENT LISTENERS
+
+
+
+
+
