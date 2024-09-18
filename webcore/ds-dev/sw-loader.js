cf5ce4cdbc6035cdb1332dcf02e17cfa296f301d
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-09-18 09:17:41 -0400

    Upload ds-dev (for dev purposes. duh)

---
+++ webcore/ds-dev/sw-loader.js
@@ -0,0 +1,28 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+        // registration worked
+        console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
+(function() {
+
+    var cnt = 0;
+    // Prompt to install PWA
+    window.onbeforeinstallprompt = function(e) {
+        cnt += 1;
+        if (cnt > 2) {
+            return;
+        }
+        console.log('Before install prompt', e);
+        e.preventDefault();
+        var deferredPrompt = e;
+        window.onclick = function(e) {
+            deferredPrompt.prompt();
+            window.onclick = null;
+        }
+    };
+})();
