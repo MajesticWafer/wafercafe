eec857de0aba1985212e141d51caaa8c428e9fb3
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-09-25 10:18:04 -0400

    Delete wasm-port directory

--- wasm-port/build.sh
+++
@@ -1,4 +0,0 @@
-SRC="main.cpp Platform.cpp"
-EXPORTED_FUNCTIONS='["_main","_getSymbol","_loadROM","_reset","_runFrame"]'
-
-emcc -O3 -s EXPORTED_FUNCTIONS=$EXPORTED_FUNCTIONS -s TOTAL_MEMORY=268435456  ../build/src/libcore.a $SRC -I ../src
