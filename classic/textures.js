3cdf1d97a4d4d2aed30320f784b92d1f436bcce6
MajesticWafer <123651667+MajesticWafer@users.noreply.github.com>
2024-10-01 06:51:53 -0400

    Add new blocks

--- classic/textures.js
+++ classic/textures.js
@@ -15,6 +15,42 @@ const textures = {
   grassSide: loader.load('assets/grass_dirt.png', (texture) => {
     texture.magFilter = THREE.NearestFilter;
     texture.minFilter = THREE.NearestFilter;
+  }),
+  bedrock: loader.load('assets/bedrock.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  gravel: loader.load('assets/gravel.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  leavesOpaque: loader.load('assets/leaves_opaque.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  rock: loader.load('assets/rock.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  sand: loader.load('assets/sand.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  stone: loader.load('assets/stone.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  treeSide: loader.load('assets/tree_side.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  treeTop: loader.load('assets/tree_top.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+  }),
+  wood: loader.load('assets/wood.png', (texture) => {
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
   })
 };
 
@@ -27,5 +63,17 @@ function createGrassBlockMaterial() {
     new THREE.MeshBasicMaterial({ map: textures.dirt }),      // bottom
     new THREE.MeshBasicMaterial({ map: textures.grassSide }), // front
     new THREE.MeshBasicMaterial({ map: textures.grassSide })  // back
+  ];
+}
+
+// Creating materials for the tree logs
+function createLogMaterial() {
+  return [
+    new THREE.MeshBasicMaterial({ map: textures.treeSide }), // left
+    new THREE.MeshBasicMaterial({ map: textures.treeSide }), // right
+    new THREE.MeshBasicMaterial({ map: textures.treeTop }),  // top
+    new THREE.MeshBasicMaterial({ map: textures.treeTop }),  // bottom
+    new THREE.MeshBasicMaterial({ map: textures.treeSide }), // front
+    new THREE.MeshBasicMaterial({ map: textures.treeSide })  // back
   ];
 }
